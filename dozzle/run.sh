337e5c6921f5851ba4cd22d032efb0ba8dfc43f0
Erreur32 <erreur32@echosystem.fr>
2025-02-24 01:22:46 +0100

    Update run.sh

--- dozzle/run.sh
+++ dozzle/run.sh
@@ -4,9 +4,12 @@
 # Print a message indicating that Dozzle is starting
 echo "Starting Dozzle..."
 
+# Start Dozzle in the foreground (correct execution)
+exec /usr/bin/dozzle --host 0.0.0.0 --port 8666
+
 # Run the Dozzle container with the necessary configurations
-docker run --rm \
-    --network=host \  # Use the host network mode to ensure full connectivity
-    -e DOZZLE_BASE=/api/panel \  # Set the base path for Dozzle when running in Home Assistant
-    -v /var/run/docker.sock:/var/run/docker.sock \  # Mount the Docker socket to allow access to logs
-    amir20/dozzle:latest
+#docker run --rm \
+#    --network=host \  # Use the host network mode to ensure full connectivity
+#    -e DOZZLE_BASE=/api/panel \  # Set the base path for Dozzle when running in Home Assistant
+#    -v /var/run/docker.sock:/var/run/docker.sock \  # Mount the Docker socket to allow access to logs
+#    amir20/dozzle:latest
