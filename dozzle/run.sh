40e3763d7440c9ce3e31a155eea1af7e7ceb448b
Erreur32 <erreur32@echosystem.fr>
2025-02-26 21:38:51 +0100

    Add files via upload

--- dozzle/run.sh
+++ dozzle/run.sh
@@ -1,14 +1,4 @@
 #!/usr/bin/with-contenv bashio
 
-# Debug : afficher les logs au démarrage
-echo "🚀 Dozzle Add-on is starting..."
-export BASHIO_LOG_LEVEL="debug"
-
-# Vérifier que l'exécutable est bien disponible
-if [ ! -f /usr/bin/dozzle ]; then
-    echo "❌ Dozzle binary not found! Exiting..."
-    exit 1
-fi
-
-# Lancer Dozzle en mode natif (sans Docker-in-Docker)
-exec /usr/bin/dozzle --host 0.0.0.0 --port 8666
+echo "Starting Dozzle..."
+exec /usr/bin/dozzle
