3b7f3358e2a8869856d1ddb7a0a01aba4f7ffa6d
Erreur32 <erreur32@echosystem.fr>
2025-02-27 10:33:42 +0100

    Update run.sh

--- dozzle/run.sh
+++ dozzle/run.sh
@@ -4,16 +4,13 @@
 echo "Starting Dozzle..."
  #!/usr/bin/with-contenv bashio
 
- 
-
 # Utilisez bashio pour lire les options de configuration
-CONFIG_PATH=/data/options.json
-
-# Exemple de lecture d'une option de configuration
 DOZZLE_LEVEL=$(bashio::config 'DOZZLE_LEVEL')
+DOZZLE_TAILSIZE=$(bashio::config 'DOZZLE_TAILSIZE')
+DOZZLE_FILTER=$(bashio::config 'DOZZLE_FILTER')
 
-# Commande pour démarrer Dozzle
-exec dozzle --level "$DOZZLE_LEVEL"
+# Commande pour démarrer Dozzle avec les options configurées
+exec dozzle --level "$DOZZLE_LEVEL" --tail "$DOZZLE_TAILSIZE" --filter "$DOZZLE_FILTER"
 
 
 
