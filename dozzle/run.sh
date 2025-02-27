f392e5443afae6c9fe2c2e40c17c5edcc34b61e7
Erreur32 <erreur32@echosystem.fr>
2025-02-27 13:50:32 +0100

    Correction des chemins dans le Dockerfile

--- dozzle/run.sh
+++ dozzle/run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/with-contenv bashio
+<<<<<<< HEAD
 # run.sh
 # Print a message indicating that Dozzle is starting
 echo "Starting Dozzle..."
@@ -16,6 +17,23 @@ exec dozzle --level "$DOZZLE_LEVEL" --tail "$DOZZLE_TAILSIZE" --filter "$DOZZLE_
 
 # Start Dozzle in the foreground (correct execution)
 #exec /usr/bin/dozzle --host 0.0.0.0 --port 8666
+=======
+
+set -e
+
+# Affiche un message dans les logs de l'add-on
+echo "Starting Dozzle on port 8099..."
+
+# Vérifier que le fichier binaire existe avant de l'exécuter
+if [ ! -f "/usr/local/bin/dozzle" ]; then
+    echo "Error: Dozzle binary not found in /usr/local/bin/dozzle"
+    exit 1
+fi
+
+# Démarrer Dozzle
+exec /usr/local/bin/dozzle --addr :8099
+
+>>>>>>> 88f893c (Mise à jour de l'addon Dozzle pour HA)
 
 # Run the Dozzle container with the necessary configurations
 #docker run --rm \
