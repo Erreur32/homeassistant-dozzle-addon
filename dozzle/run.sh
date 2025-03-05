cb2a496703c7272b09a6beb708657dda46e43a8d
Erreur32 <erreur32@echosystem.fr>
2025-03-05 15:17:54 +0100

    🔧Fixouille

--- dozzle/run.sh
+++ dozzle/run.sh
@@ -8,9 +8,8 @@ REMOTE_ACCESS=$(bashio::config 'remote_access')
 DOZZLE_AGENT_ENABLED=$(bashio::config 'dozzle_agent_enabled')
 DOZZLE_AGENT_PORT=$(bashio::config 'dozzle_agent_port')
 
-# Internal Dozzle ports
-INTERNAL_PORT_INGRESS=8080
-INTERNAL_PORT_EXTERNAL=8081
+# Internal Dozzle port
+INTERNAL_PORT=8080
 
 # Default external port (will be changed by HA if not available)
 DEFAULT_EXTERNAL_PORT=8099
@@ -45,8 +44,8 @@ bashio::log.info "Ingress entry point: '${INGRESS_ENTRY}'"
 # Trim whitespace from INGRESS_ENTRY
 INGRESS_ENTRY=$(echo "${INGRESS_ENTRY}" | xargs)
 
-# Start Ingress instance with unique ID and no analytics
-CMD_INGRESS="dozzle --addr 0.0.0.0:${INTERNAL_PORT_INGRESS} --id dozzle_ingress --no-analytics"
+# Start Ingress instance with namespace and no analytics
+CMD_INGRESS="dozzle --addr 0.0.0.0:${INTERNAL_PORT} --namespace dozzle_ingress --no-analytics"
 if [[ -n "${INGRESS_ENTRY}" ]]; then
     bashio::log.info "Using base path for Ingress: '${INGRESS_ENTRY}'"
     CMD_INGRESS="${CMD_INGRESS} --base ${INGRESS_ENTRY}"
@@ -54,8 +53,8 @@ fi
 
 # Start External instance if enabled
 if [[ "${REMOTE_ACCESS}" = "true" ]]; then
-    # External instance with unique ID and no analytics
-    CMD_EXTERNAL="dozzle --addr 0.0.0.0:${INTERNAL_PORT_EXTERNAL} --id dozzle_external --no-analytics"
+    # External instance with namespace and no analytics
+    CMD_EXTERNAL="dozzle --addr 0.0.0.0:${INTERNAL_PORT} --namespace dozzle_external --no-analytics"
     if [[ -n "${EXTERNAL_PORT}" ]]; then
         bashio::log.info "Remote access enabled - External port: ${EXTERNAL_PORT}"
         if [[ "${EXTERNAL_PORT}" != "${DEFAULT_EXTERNAL_PORT}" ]]; then
@@ -74,13 +73,13 @@ if [[ "${DOZZLE_AGENT_ENABLED}" = "true" ]]; then
 fi
 
 # Start Dozzle instances
-bashio::log.info "Starting Dozzle Ingress instance on port ${INTERNAL_PORT_INGRESS}"
+bashio::log.info "Starting Dozzle Ingress instance on port ${INTERNAL_PORT}"
 bashio::log.debug "Ingress Command: ${CMD_INGRESS}"
 ${CMD_INGRESS} &
 PID_INGRESS=$!
 
 if [[ "${REMOTE_ACCESS}" = "true" ]]; then
-    bashio::log.info "Starting Dozzle External instance on port ${INTERNAL_PORT_EXTERNAL}"
+    bashio::log.info "Starting Dozzle External instance on port ${INTERNAL_PORT}"
     bashio::log.debug "External Command: ${CMD_EXTERNAL}"
     ${CMD_EXTERNAL} &
     PID_EXTERNAL=$!
